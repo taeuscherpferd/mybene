18aff2d1b032d3531d7672a68ea3f37288f88aed
Casey Deccio <casey@deccio.net>
2018-12-03 23:36:36 -0700

    Add stub switch

---
+++ bene/switch.py
@@ -0,0 +1,37 @@
+import copy
+import logging
+
+from .node import Node
+
+logger = logging.getLogger(__name__)
+
+class Switch(Node):
+    def __init__(self, hostname):
+        self.hostname = hostname
+        self.links = []
+        self.forwarding_table = {}
+
+    # -- Links --
+
+    def add_link(self, link):
+        self.links.append(link)
+
+    def delete_link(self, link):
+        if link not in self.links:
+            return
+        self.links.remove(link)
+
+    def get_link(self, name):
+        for link in self.links:
+            if link.endpoint.hostname == name:
+                return link
+        return None
+
+    def receive_packet(self, (packet, link)):
+        pass
+
+    def forward_packet(self, packet):
+        pass
+
+    def forward_broadcast_packet(self, packet):
+        pass
